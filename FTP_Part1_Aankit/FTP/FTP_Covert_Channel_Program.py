0bf373979207538dcb2f9b8dbb4919098290dacc
Xenipulator <apo091@latech.edu>
2021-03-30 17:35:32 -0500

    FTP code that gets permissions, converts to binary, filters them, and converts to ascii

---
+++ FTP_Part1_Aankit/FTP/FTP_Covert_Channel_Program.py
@@ -0,0 +1,43 @@
+import os
+import stat
+
+filePermissions = []
+
+#https://stackoverflow.com/questions/1861836/checking-file-permissions-in-linux-with-python
+#print(bin(os.stat("/home/xenipulator/Desktop/CYEN 301/Assignments/FTP/ftp.pdf").st_mode))
+
+#create an array to hold names of files in directory
+banana = os.listdir("/home/xenipulator/Desktop/CYEN 301/Assignments/FTP")
+
+#print array of files
+print (banana)
+
+for i in banana:
+    path = "/home/xenipulator/Desktop/CYEN 301/Assignments/FTP/"+i
+
+    filePermissions.append(bin(os.stat(path).st_mode)[8:])  #adds binary representation of permissions 
+                                                            #to the file permissions array using the path
+                                                            #the [8:] is to isolate the permissions portion
+
+print (filePermissions) #prints array of file permission binary
+
+usethese = []
+
+#this next part goes through the file permissions 
+#array and checks the first 3 boolean values
+#if the first 3 are not set, then append those in
+#the use these array 
+for g in filePermissions:
+    if(g[:3] == "000"):
+        usethese.append(g[3:])
+
+print("\n\n")
+print(usethese)
+
+message = ""
+
+for i in usethese:
+    n = int(i,2)
+    message+=n.to_bytes((n.bit_length() + 6) // 7, 'big').decode()
+
+print(message)
