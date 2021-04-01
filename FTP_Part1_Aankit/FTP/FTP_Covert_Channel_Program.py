f0426ff0f600973f7b45587e83c9ccc3cc1dd997
Xenipulator <apo091@latech.edu>
2021-04-01 02:55:45 -0500

    FTP Code edited added METHOD to differentiate from 10 and 7 bit

--- FTP_Part1_Aankit/FTP/FTP_Covert_Channel_Program.py
+++ FTP_Part1_Aankit/FTP/FTP_Covert_Channel_Program.py
@@ -1,6 +1,7 @@
 import os   #allows me to access os functions
 import stat #interprets from os library
 
+Method = True#if true, 7 bit, else 10 bit
 filePermissions = []
 
 #https://stackoverflow.com/questions/1861836/checking-file-permissions-in-linux-with-python
@@ -8,34 +9,45 @@ filePermissions = []
 
 #create an array to hold names of files in directory
 banana = os.listdir("/home/xenipulator/Desktop/CYEN 301/Assignments/FTP")
+usethese = []
 
 #print array of files
 print (banana)
 
-for i in banana:
-    path = "/home/xenipulator/Desktop/CYEN 301/Assignments/FTP/"+i
+if(Method):
+    for i in banana:
+        path = "/home/xenipulator/Desktop/CYEN 301/Assignments/FTP/"+i
 
-    filePermissions.append(bin(os.stat(path).st_mode)[8:])  #adds binary representation of permissions 
-                                                            #to the file permissions array using the path
-                                                            #the [8:] is to isolate the permissions portion
+        filePermissions.append(bin(os.stat(path).st_mode)[8:])  #adds binary representation of permissions 
+                                                                #to the file permissions array using the path
+                                                                #the [8:] is to isolate to the final 8 bits
+        #this next part goes through the file permissions 
+        #array and checks the first 3 boolean values
+        #if the first 3 are not set, then append those in
+        #the use these array 
+        for g in filePermissions:
+            if(g[:3] == "000"):
+                usethese.append(g[3:])
+
+if(!Method):
+    for i in banana:
+        path = "/home/xenipulator/Desktop/CYEN 301/Assignments/FTP/"+i
+
+        filePermissions.append(bin(os.stat(path).st_mode)[6:])  #adds binary representation of permissions 
+                                                                #to the file permissions array using the path
+                                                                #the [6:] is to isolate to the final 10 bits
+        for g in filePermissions:
+            usethese.append(g)
 
 print (filePermissions) #prints array of file permission binary
 
-usethese = []
-
-#this next part goes through the file permissions 
-#array and checks the first 3 boolean values
-#if the first 3 are not set, then append those in
-#the use these array 
-for g in filePermissions:
-    if(g[:3] == "000"):
-        usethese.append(g[3:])
 
 print("\n\n")
 print(usethese)
 
 message = ""
 
+#gotta edit lines after this for the 10 bit, prolly add an if statement
 for i in usethese:
     n = int(i,2)
     message+=n.to_bytes((n.bit_length() + 6) // 7, 'big').decode()
